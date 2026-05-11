c04362171f4691c631540b81a8565748cf0e15de
Ling <nlinh10082007@gmail.com>
2026-05-12 03:48:12 +0700

    1

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,20 @@
-module com.example.btl {
+module com.btl.n8 {
+
     requires javafx.controls;
     requires javafx.fxml;
 
+    requires java.sql;
+    requires mysql.connector.j;
+    requires com.google.gson;
+    opens com.btl.n8 to javafx.fxml;
+    exports com.btl.n8;
+    exports com.btl.n8.Controller;
+    opens com.btl.n8.Controller to javafx.fxml;
+    exports com.btl.n8.DTO;
+    exports com.btl.n8.Network;
+    exports com.btl.n8.util;
+    exports com.btl.n8.Model;
+    exports com.btl.n8.Service;
+    exports com.btl.n8.Connection;
 
-    opens com.example.btl to javafx.fxml;
-    exports com.example.btl;
 }
